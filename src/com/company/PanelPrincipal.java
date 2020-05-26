3b664dbf72fb2061cad0a9bc3c603424437ffc1b
Moises <fabmoy1866@gmail.com>
2020-05-25 19:57:43 -0500

    Added new changes to the main panel class, defined actions to each jbutton

--- src/com/company/PanelPrincipal.java
+++ src/com/company/PanelPrincipal.java
@@ -15,13 +15,41 @@ public class PanelPrincipal extends JPanel {
     private JTextArea usersArea =new JTextArea(1,5);
     private JButton buttonTask =new JButton("ADD NEW TASK");
     private JButton buttonUser =new JButton("ADD USER");
+    private JButton randomButton = new JButton("RANDOMIZE !");
+    private Component thisComp=this;
 
 
     public PanelPrincipal (){
         super.setLayout(new GridLayout(2,2));
         add (new PanelTasks());
         addActionToButtons();
+        add (new RandomPanel());
+
     }
+
+    private class RandomPanel extends JPanel {
+        public RandomPanel (){
+            randomButton.setPreferredSize(new Dimension(250,60));
+            randomButton.setForeground(Color.RED);
+            randomButton.setBackground(Color.WHITE);
+            randomButton.addActionListener(new MainAction());
+            add (randomButton);
+        }
+    }
+
+    private class MainAction implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            if (tasksStack.isEmpty()||usersArray.isEmpty()){
+                JOptionPane.showMessageDialog(thisComp,"ERROR, USERS OR TASKS ARRAY IS EMPTY");
+            }else {
+                //MAIN ACTION
+            }
+        }
+    }
+
+
 
     private void addActionToButtons(){
         buttonTask.addActionListener(new ActionListener() {
--- src/com/company/VentanaPrincipal.java
+++ src/com/company/VentanaPrincipal.java
@@ -6,6 +6,7 @@ public class VentanaPrincipal extends JFrame {
     public VentanaPrincipal (){
         super.setBounds(200,200,700,300);
         setLocationRelativeTo(null);
+        super.setResizable(false);
         super.setTitle("Randomizer");
         super.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         add (new PanelPrincipal());
